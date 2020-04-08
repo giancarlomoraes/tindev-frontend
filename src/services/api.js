74ac6ced0ebd141f1676a402cae2082f39ecd9ae
Giancarlo Moraes <giancarlomoraes@hotmail.com>
2020-04-08 02:11:42 -0300

    Adiciona variável de ambiente para URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://trouble.surge.sh'
-});
-
-axios.defaults.baseURL = 'http://grubby-dinosaurs.surge.sh/';
-axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    baseURL: process.env.REACT_APP_API_URL
+}); 
 
 export default api; 
